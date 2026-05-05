7499e725bc9a23323b0bd3ccba831cb8a867c25a
Olaoye kunle <51406569+kunle1984@users.noreply.github.com>
2026-05-05 11:10:55 +0100

    adjusted callback

---
+++ dist/assets/AdminDashboard-CIaROpfT.js
@@ -0,0 +1,1 @@
+import{j as e,x as s,z as t}from"./index-BPU50WrA.js";import{L as o}from"./Layout-BfIlKv9J.js";import{A as r}from"./AdminHome-DeHDsbA2.js";function i(){return e.jsx(s,{children:e.jsx(t,{path:"/",element:e.jsx(o,{}),children:e.jsx(t,{index:!0,element:e.jsx(r,{})})})})}export{i as default};
