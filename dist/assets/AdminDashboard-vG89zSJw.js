435c960a043b381ef1139cfde8b2b2e71ebd1ce6
Olaoye kunle <51406569+kunle1984@users.noreply.github.com>
2026-05-06 10:39:06 +0100

    added bankCode

---
+++ dist/assets/AdminDashboard-vG89zSJw.js
@@ -0,0 +1,1 @@
+import{j as e,x as s,z as t}from"./index-0j0SoMgD.js";import{L as o}from"./Layout-BKGkCwm2.js";import{A as r}from"./AdminHome-v198-9bn.js";function i(){return e.jsx(s,{children:e.jsx(t,{path:"/",element:e.jsx(o,{}),children:e.jsx(t,{index:!0,element:e.jsx(r,{})})})})}export{i as default};
